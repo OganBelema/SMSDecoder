4a508e973db1d6a97f05ae8d755d95352fa81e42
Belema Ogan <Oganbelema@gmail.com>
2019-04-26 17:01:56 +0100

    Create Receiver
    - Add required permissions in manifest file for reading sms
    - Create SMSReceiver broadcast listener to be notified when an sms is
    received
    - Register receiver for sms messages in manifest file
    - Add intent filter for SMS received
    - Rename MainActivity to SMSReceiverActivity to represent what it is
    better

---
+++ app/src/main/java/com/oganbelema/smsdecoder/SMSReceiver.java
@@ -0,0 +1,18 @@
+package com.oganbelema.smsdecoder;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ * This is a broadcast receiver to receive SMS messages and check if it is
+ * from a specified contact and if it is start the SMSReceiverActivity
+ * @author Belema Ogan
+ */
+public class SMSReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+    }
+}
--- app/src/main/java/com/oganbelema/smsdecoder/SMSReceiverActivity.java
+++ app/src/main/java/com/oganbelema/smsdecoder/SMSReceiverActivity.java
@@ -3,12 +3,16 @@ package com.oganbelema.smsdecoder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+/**
+ * This is an activity that decodes an sms and decodes it
+ * @author Belema Ogan
+ */
+public class SMSReceiverActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_sms_receiver);
 
     }
 }
