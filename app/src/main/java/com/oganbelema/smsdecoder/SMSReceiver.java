93ec8e04d9135c9899a80ca0341ea7c7abcc5ac2
Belema Ogan <Oganbelema@gmail.com>
2019-04-26 21:19:44 +0100

    Start activity with received sms body
    - Add code to the SMSReceiver's onReceive method to process sms received
    - Add permission to read contact to manifest file
    - Add required contact to the string file
    - Add method to read contact and check that the sender's contact is the required contact
    - Add method to start SMSReceiverActivity with the received sms body
    - Add method to check if app has required permissions in SMSReceiverActivity
    - Add method to request for required permissions in SMSReceiverActivity
    - Add code to receive sms body from SMSReceiveActivity intent

--- app/src/main/java/com/oganbelema/smsdecoder/SMSReceiver.java
+++ app/src/main/java/com/oganbelema/smsdecoder/SMSReceiver.java
@@ -1,8 +1,17 @@
 package com.oganbelema.smsdecoder;
 
+import android.annotation.SuppressLint;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.telephony.SmsMessage;
+import android.util.Log;
+import android.widget.Toast;
 
 /**
  * This is a broadcast receiver to receive SMS messages and check if it is
@@ -11,8 +20,91 @@ import android.content.Intent;
  */
 public class SMSReceiver extends BroadcastReceiver {
 
+    private static final String TAG = SMSReceiver.class.getSimpleName();
+
+    public static final String pdu_type = "pdus";
+
+    public static final String SMS_BODY_KEY = "message_key";
+
+    @SuppressLint("NewApi")
     @Override
     public void onReceive(Context context, Intent intent) {
+        // Get the SMS message.
+        Bundle bundle = intent.getExtras();
 
+        if (bundle != null){
+            SmsMessage[] smsMessages;
+            String format = bundle.getString("format");
+
+            // Retrieve the SMS message received.
+            Object[] pdus = (Object[]) bundle.get(pdu_type);
+            if (pdus != null) {
+
+                // Check the Android version.
+                boolean isVersionM = (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M);
+
+                // Fill the smsMessages array.
+                smsMessages = new SmsMessage[pdus.length];
+                for (int i = 0; i < smsMessages.length; i++) {
+                    // Check Android version and use appropriate createFromPdu.
+                    if (isVersionM) {
+                        // If Android version M or newer:
+                        smsMessages[i] = SmsMessage.createFromPdu((byte[]) pdus[i], format);
+                    } else {
+                        // If Android version L or older:
+                        smsMessages[i] = SmsMessage.createFromPdu((byte[]) pdus[i]);
+                    }
+
+                    String senderNumber = smsMessages[i].getOriginatingAddress();
+                    String messageBody = smsMessages[i].getMessageBody();
+
+                    if (isRequiredContact(context, senderNumber)){
+                        startSMSReceiverActivity(context, messageBody);
+                    }
+
+                }
+            }
+        }
+    }
+
+    /**
+     * Checks if the contact of the sms sender is the required contact
+     * @param context used to create a cursor object to query contact list
+     * @param senderNumber the number of the sender. Used to build the uri to query the contact list
+     * @return true if contact is required contact or false if it isn't or number is not saved
+     */
+    private boolean isRequiredContact(Context context, String senderNumber){
+        String contactName = "";
+
+        //Resolving the contact name from the contacts.
+        Uri lookupUri = Uri.withAppendedPath(ContactsContract.PhoneLookup.CONTENT_FILTER_URI, Uri.encode(senderNumber));
+        Cursor cursor = context.getContentResolver().query(lookupUri, new String[]{ContactsContract.Data.DISPLAY_NAME},null,null,null);
+
+        if (cursor != null){
+            try {
+                cursor.moveToFirst();
+                contactName = cursor.getString(0);
+                Toast.makeText(context, contactName, Toast.LENGTH_LONG).show();
+            } catch (Exception e) {
+                Log.e(TAG, "Error reading contact", e);
+            }finally{
+                cursor.close();
+            }
+        }
+
+        return contactName.equals(context.getString(R.string.ven_ten_contact));
+    }
+
+    /**
+     * Starts the SMSReceiverActivity with the received sms body
+     * @param context used to create intent and start activity
+     * @param messageBody the body of the sms message received
+     */
+    private void startSMSReceiverActivity(Context context, String messageBody) {
+        Intent startMainActivityIntent = new Intent(context, SMSReceiverActivity.class);
+        startMainActivityIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startMainActivityIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startMainActivityIntent.putExtra(SMS_BODY_KEY, messageBody);
+        context.startActivity(startMainActivityIntent);
     }
 }
--- app/src/main/java/com/oganbelema/smsdecoder/SMSReceiverActivity.java
+++ app/src/main/java/com/oganbelema/smsdecoder/SMSReceiverActivity.java
@@ -1,18 +1,69 @@
 package com.oganbelema.smsdecoder;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 /**
- * This is an activity that decodes an sms and decodes it
+ * This is an activity that receives an sms and decodes it
  * @author Belema Ogan
  */
 public class SMSReceiverActivity extends AppCompatActivity {
+
+    private final String TAG = SMSReceiver.class.getSimpleName();
+
+    /**
+     * Request permission code
+     */
+    private static final int REQUEST_PERMISSION_CODE = 101;
+
+    /**
+     * Array of required permissions
+     */
+    private final String[] mPermissions = {
+            Manifest.permission.READ_SMS,
+            Manifest.permission.RECEIVE_SMS,
+            Manifest.permission.READ_CONTACTS
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sms_receiver);
 
+        if (!hasPermissions(this, mPermissions)){
+            ActivityCompat.requestPermissions(this, mPermissions, REQUEST_PERMISSION_CODE);
+        }
+
+        Bundle bundle = getIntent().getExtras();
+
+        if (bundle != null){
+
+            String message = bundle.getString(SMSReceiver.SMS_BODY_KEY);
+
+
+        }
+
+    }
+
+    /**
+     * Check that the app has the required permissions
+     * @param context activity's context
+     * @param permissions list of required permissions
+     * @return false if app does not have permission or true if it has
+     */
+    private static boolean hasPermissions(Context context, String... permissions){
+        if (context != null && permissions != null){
+            for (String permission : permissions){
+                if (ActivityCompat.checkSelfPermission(context, permission)
+                        != PackageManager.PERMISSION_GRANTED){
+                    return  false;
+                }
+            }
+        }
+        return true;
     }
 }
