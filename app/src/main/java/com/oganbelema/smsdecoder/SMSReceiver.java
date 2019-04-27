6dd7125f60c32363a75cfcce80125700ef8bd276
Belema Ogan <Oganbelema@gmail.com>
2019-04-27 07:14:58 +0100

    Extract key information and populate view
    - Create regex for sms body
    - Extract key information from sms body using regex pattern matcher
    - Create views for the extracted information
    - Populate the view with extracted information
    - Create strings in string file for date, time and dimension text
    - Create string for message from ven10 text
    - Update actionbar to show sender of message
    - Create border drawable for coded message and date and time text view
    - Add screenshots of the app

--- app/src/main/java/com/oganbelema/smsdecoder/SMSReceiver.java
+++ app/src/main/java/com/oganbelema/smsdecoder/SMSReceiver.java
@@ -22,47 +22,56 @@ public class SMSReceiver extends BroadcastReceiver {
 
     private static final String TAG = SMSReceiver.class.getSimpleName();
 
-    public static final String pdu_type = "pdus";
+    public static final String PDU_TYPE = "pdus";
 
     public static final String SMS_BODY_KEY = "message_key";
 
-    @SuppressLint("NewApi")
     @Override
     public void onReceive(Context context, Intent intent) {
         // Get the SMS message.
         Bundle bundle = intent.getExtras();
 
         if (bundle != null){
-            SmsMessage[] smsMessages;
-            String format = bundle.getString("format");
+            processMessage(context, bundle);
+        }
+    }
 
-            // Retrieve the SMS message received.
-            Object[] pdus = (Object[]) bundle.get(pdu_type);
-            if (pdus != null) {
+    /**
+     * Processes the messages contained in the received bundle
+     * @param context application's context
+     * @param bundle the extras in the received intent
+     */
+    @SuppressLint("NewApi")
+    private void processMessage(Context context, Bundle bundle) {
+        SmsMessage[] smsMessages;
+        String format = bundle.getString("format");
 
-                // Check the Android version.
-                boolean isVersionM = (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M);
+        // Retrieve the SMS message received.
+        Object[] pdus = (Object[]) bundle.get(PDU_TYPE);
+        if (pdus != null) {
 
-                // Fill the smsMessages array.
-                smsMessages = new SmsMessage[pdus.length];
-                for (int i = 0; i < smsMessages.length; i++) {
-                    // Check Android version and use appropriate createFromPdu.
-                    if (isVersionM) {
-                        // If Android version M or newer:
-                        smsMessages[i] = SmsMessage.createFromPdu((byte[]) pdus[i], format);
-                    } else {
-                        // If Android version L or older:
-                        smsMessages[i] = SmsMessage.createFromPdu((byte[]) pdus[i]);
-                    }
+            // Check the Android version.
+            boolean isVersionM = (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M);
 
-                    String senderNumber = smsMessages[i].getOriginatingAddress();
-                    String messageBody = smsMessages[i].getMessageBody();
-
-                    if (isRequiredContact(context, senderNumber)){
-                        startSMSReceiverActivity(context, messageBody);
-                    }
-
+            // Fill the smsMessages array.
+            smsMessages = new SmsMessage[pdus.length];
+            for (int i = 0; i < smsMessages.length; i++) {
+                // Check Android version and use appropriate createFromPdu.
+                if (isVersionM) {
+                    // If Android version M or newer:
+                    smsMessages[i] = SmsMessage.createFromPdu((byte[]) pdus[i], format);
+                } else {
+                    // If Android version L or older:
+                    smsMessages[i] = SmsMessage.createFromPdu((byte[]) pdus[i]);
                 }
+
+                String senderNumber = smsMessages[i].getOriginatingAddress();
+                String messageBody = smsMessages[i].getMessageBody();
+
+                if (isRequiredContact(context, senderNumber)){
+                    startSMSReceiverActivity(context, messageBody);
+                }
+
             }
         }
     }
--- app/src/main/java/com/oganbelema/smsdecoder/SMSReceiverActivity.java
+++ app/src/main/java/com/oganbelema/smsdecoder/SMSReceiverActivity.java
@@ -3,12 +3,24 @@ package com.oganbelema.smsdecoder;
 import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.graphics.Color;
 import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.util.TypedValue;
+import android.view.View;
+import android.widget.TextView;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Locale;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
- * This is an activity that receives an sms and decodes it
+ * This is an activity that receives an sms, decodes it and builds it's ui with decoded message
  * @author Belema Ogan
  */
 public class SMSReceiverActivity extends AppCompatActivity {
@@ -29,10 +41,79 @@ public class SMSReceiverActivity extends AppCompatActivity {
             Manifest.permission.READ_CONTACTS
     };
 
+    /**
+     * Regex of the expected SMS message
+     */
+    String nonGreedyRegex =".*?";	// Non-greedy match on filler
+    String dateRegex ="(11\\/18\\/2016)";	// MMDDYYYY 1
+    String timeRegex ="((?:(?:[0-1][0-9])|(?:[2][0-3])|(?:[0-9])):(?:[0-5][0-9])(?::[0-5][0-9])" +
+            "?(?:\\s?(?:am|AM|pm|PM))?)";	// HourMinuteSec 1
+    String re5="( )";	// White Space 1
+    String re6="(TXG)";	// Word 1
+    String re7="(\\s+)";	// White Space 2
+    String re8="(C55)";	// Alphanum 1
+    String re9="(\\/DRAINQ)";	// Unix Path 1
+    String re10="(.)";	// Any Single Character 1
+    String re11="( )";	// White Space 3
+    String re12="(WHITE)";	// Word 2
+    String re13="(\\/GIN)";	// Unix Path 2
+    String re14="(~)";	// Any Single Character 2
+    String widthRegex ="(200)";	// Integer Number 1
+    String lengthRegex ="(200)";	// Integer Number 2
+    String color1 ="(CD00A0)";	// Alphanum 2
+    String color2 ="(FFFFFF)";	// Word 3
+
+    /**
+     * Pattern for the date
+     */
+    Pattern mDatePattern = Pattern.compile(nonGreedyRegex+dateRegex,Pattern.CASE_INSENSITIVE | Pattern.DOTALL);
+
+    /**
+     * Pattern for the time
+     */
+    Pattern mTimePattern = Pattern.compile(nonGreedyRegex+timeRegex,Pattern.CASE_INSENSITIVE | Pattern.DOTALL);
+
+    /**
+     * Pattern for width and length
+     */
+    Pattern mWidthAndLengthPattern = Pattern.compile(nonGreedyRegex+ widthRegex +nonGreedyRegex+ lengthRegex,Pattern.CASE_INSENSITIVE | Pattern.DOTALL);
+
+    /**
+     * Pattern for the color
+     */
+    Pattern mColorsPattern = Pattern.compile(nonGreedyRegex+ color1 +nonGreedyRegex+ color2,Pattern.CASE_INSENSITIVE | Pattern.DOTALL);
+
+    /**
+     * Pattern for the coded message
+     */
+    Pattern mCodedMessagePattern = Pattern.compile(nonGreedyRegex+re6+re5+re8+re9+re10+re11+re12+re13+re14,Pattern.CASE_INSENSITIVE | Pattern.DOTALL);
+
+
+    /**
+     * TextView for dimension
+     */
+    private TextView mDimensionTextView;
+
+    /**
+     * TextView for coded message
+     */
+    private TextView mCodedMessageTextView;
+
+    /**
+     * TextView for date and time
+     */
+    private TextView mDateAndTimeTextView;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sms_receiver);
+
+        TextView awaitingMessageTextView = findViewById(R.id.awaiting_message_textView);
+        mDimensionTextView = findViewById(R.id.dimension_textView);
+        mCodedMessageTextView = findViewById(R.id.coded_message_textView);
+        mDateAndTimeTextView = findViewById(R.id.date_time_textView);
 
         if (!hasPermissions(this, mPermissions)){
             ActivityCompat.requestPermissions(this, mPermissions, REQUEST_PERMISSION_CODE);
@@ -44,9 +125,158 @@ public class SMSReceiverActivity extends AppCompatActivity {
 
             String message = bundle.getString(SMSReceiver.SMS_BODY_KEY);
 
+            if (message != null){
+                awaitingMessageTextView.setVisibility(View.GONE);
+
+                showSenderInActionBar();
+
+                extractWidthAndLength(message);
+
+                extractColors(message);
+
+                extractCodedMessage(message);
+
+                extractDateAndTime(message);
+            }
 
         }
 
+    }
+
+    /**
+     * This method extracts the coded message from the received sms message using regex pattern
+     * matcher
+     * @param message the received sms message body
+     */
+    private void extractCodedMessage(String message) {
+        Matcher codedMessageMatcher = mCodedMessagePattern.matcher(message);
+
+        if (codedMessageMatcher.find()){
+            String codedMessage ="";
+            codedMessage += codedMessageMatcher.group(1);
+            codedMessage+= codedMessageMatcher.group(2);
+            codedMessage += codedMessageMatcher.group(3);
+            codedMessage += codedMessageMatcher.group(4);
+            codedMessage += codedMessageMatcher.group(5);
+            codedMessage += codedMessageMatcher.group(6);
+            codedMessage += codedMessageMatcher.group(7);
+            displayCodedMessage(codedMessage);
+        }
+    }
+
+    /**
+     * Extracts the colors from the received sms message using regex pattern matcher
+     * @param message the received sms message body
+     */
+    private void extractColors(String message) {
+        Matcher colorMatcher = mColorsPattern.matcher(message);
+
+        if (colorMatcher.find()){
+            String color1 = colorMatcher.group(1);
+            String color2 = colorMatcher.group(2);
+            styleDimensionTextView(color1, color2);
+        }
+    }
+
+    /**
+     * Extracts the date and time from the received sms message using regex pattern matcher
+     * @param message the received sms message body
+     */
+    private void extractDateAndTime(String message) {
+        Matcher dateMatcher = mDatePattern.matcher(message);
+        Matcher timeMatcher = mTimePattern.matcher(message);
+
+        String date = "";
+        String time = "";
+
+        if (dateMatcher.find()){
+            date = dateMatcher.group(1);
+        }
+
+        if (timeMatcher.find()){
+            time = timeMatcher.group(1);
+        }
+
+        displayDateAndTime(date, time);
+    }
+
+    /**
+     * Extracts the width and length from the received sms message body using regex pattern matcher
+     * @param message the received sms message body
+     */
+    private void extractWidthAndLength(String message) {
+        Matcher widthAndLengthMatcher = mWidthAndLengthPattern.matcher(message);
+        if (widthAndLengthMatcher.find()){
+            String width = widthAndLengthMatcher.group(1);
+            String length = widthAndLengthMatcher.group(2);
+            displayDimensionTextView(width, length);
+        }
+    }
+
+    /**
+     * Displays the name of the sms sender in the actionbar
+     */
+    private void showSenderInActionBar() {
+        ActionBar actionBar = getSupportActionBar();
+
+        if (actionBar != null){
+            getSupportActionBar().setSubtitle(getString(R.string.message_from));
+        }
+    }
+
+    /**
+     * Displays the coded message in the text view
+     * @param codedMessage coded message received from SMS
+     */
+    private void displayCodedMessage(String codedMessage) {
+        mCodedMessageTextView.setText(codedMessage);
+        mCodedMessageTextView.setVisibility(View.VISIBLE);
+    }
+
+    /**
+     * Styles the dimension text background and text with the received colors
+     * @param color1 the first extracted color
+     * @param color2 the second extracted color
+     */
+    private void styleDimensionTextView(String color1, String color2) {
+        mDimensionTextView.setBackgroundColor(Color.parseColor("#"+color1));
+        mDimensionTextView.setTextColor(Color.parseColor("#"+color2));
+    }
+
+    /**
+     * Sets up the dimensions textview with the dimensions decoded and sets it's text
+     * @param width the extracted width
+     * @param length the extracted length
+     */
+    private void displayDimensionTextView(String width, String length) {
+        try {
+            int widthInInteger = Integer.valueOf(width);
+            int lengthInInteger = Integer.valueOf(length);
+            mDimensionTextView.setWidth(dpToPixel(widthInInteger));
+            mDimensionTextView.setHeight(dpToPixel(lengthInInteger));
+            mDimensionTextView.setText(getString(R.string.width_and_length, width, length));
+            mDimensionTextView.setVisibility(View.VISIBLE);
+        } catch (Exception e){
+            Log.e(TAG, "Error parsing dimension", e);
+        }
+    }
+
+    /**
+     * Formats the date and populates the view with the date and time
+     * @param date the extracted date
+     * @param time the extracted time
+     */
+    private void displayDateAndTime(String date, String time) {
+        SimpleDateFormat newFormat = new SimpleDateFormat("dd MMMM yyyy", Locale.getDefault());
+        SimpleDateFormat oldFormat = new SimpleDateFormat("MM/dd/yyyy", Locale.getDefault());
+        String formattedDate = null;
+        try {
+            formattedDate = newFormat.format(oldFormat.parse(date));
+        } catch (ParseException e) {
+            Log.e(TAG, "Error parsing date", e);
+        }
+        mDateAndTimeTextView.setText(getString(R.string.date_and_time, formattedDate, time));
+        mDateAndTimeTextView.setVisibility(View.VISIBLE);
     }
 
     /**
@@ -65,5 +295,15 @@ public class SMSReceiverActivity extends AppCompatActivity {
             }
         }
         return true;
+    }
+
+    /**
+     * Converts integer density independent pixel to pixel
+     * @param dp density independent pixel to be converted
+     * @return density independent pixel in pixel
+     */
+    private int dpToPixel(int dp){
+        return Math.round(TypedValue.applyDimension(
+                TypedValue.COMPLEX_UNIT_DIP, dp,getResources().getDisplayMetrics()));
     }
 }
